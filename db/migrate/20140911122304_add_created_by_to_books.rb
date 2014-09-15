c3725533b415a53bd09d61ec4812491ffe33184c
Riaz Virani <riaz.n.virani@gmail.com>
2014-09-15 13:36:43 -0400

    Removed migrations, updated gemefile with rails 4.1.5, and added user seed data

--- db/migrate/20140911122304_add_created_by_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedByToBooks < ActiveRecord::Migration
-  def change
-    add_column :books, :created_by, :integer
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.create!(email: "riaz.n.virani@gmail.com", password: "password")
