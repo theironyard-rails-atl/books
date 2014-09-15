854e2681e5e7a2214e0c11e67634581b54c7a43d
Riaz Virani <riaz.n.virani@gmail.com>
2014-09-15 17:53:03 -0400

    Created categories models, associations, and saved categories on google books lookup

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,28 @@
 class Book < ActiveRecord::Base
 
   validates_presence_of :title, :author, :isbn, :description
+  has_and_belongs_to_many :categories
 
   def book_data_lookup
     return false unless self.isbn
     data = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=+isbn:#{self.isbn}")
     if data["totalItems"] == 1
-      item = data["items"][0]
-      self.title ||= item["volumeInfo"]["title"]
-      self.author ||= item["volumeInfo"]["authors"][0]
-      self.description ||= item["volumeInfo"]["description"]
-      if item["volumeInfo"]["imageLinks"]
-        self.image_url = item["volumeInfo"]["imageLinks"]["thumbnail"]
+      item = data["items"][0]["volumeInfo"]
+      self.title ||= item["title"]
+      self.author ||= item["authors"][0]
+      self.description ||= item["description"]
+      if item["imageLinks"]
+        self.image_url = item["imageLinks"]["thumbnail"]
+      end
+      item["categories"].each do |category_string|
+        binding.pry
+        category = Category.find_by_name category_string
+        if category
+          self.categories.push category
+        else
+          self.categories.new(name: category_string)
+        end
+
       end
     end
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  validates_uniqueness_of :name
+  has_and_belongs_to_many :books
 end
