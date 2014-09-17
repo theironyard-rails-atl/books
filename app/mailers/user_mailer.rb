88d3f6e993c1efdd5b333e95d0f42b9f94aea8e2
James Dabbs <jamesdabbs@gmail.com>
2014-09-17 10:36:04 -0400

    Implemented all mailer functions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,13 @@ class UserMailer < ActionMailer::Base
   def ping_user user
     @user = user
     @book = Book.first
+
+    # Mailers have attachments
+    #   Not visible in body, just an attachment
+    #attachments['cute-attachment.gif'] = File.read(Rails.root.join 'public', 'cute.gif')
+    #   For this to be visible inline, do -
+    attachments.inline['cute.gif'] = File.read(Rails.root.join 'public', 'cute.gif')
+
     mail(to: user.email, subject: 'Ping!')
   end
 end
