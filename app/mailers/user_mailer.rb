6acc42d26fd3a2fda52d45c60742c02911a91e6c
James Dabbs <jamesdabbs@gmail.com>
2014-09-17 10:19:47 -0400

    Multipart email with links

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
+  default from: 'no-reply@book-project.example.com'
+
   def ping_user user
-    # Get data
+    @user = user
+    @book = Book.first
     mail(to: user.email, subject: 'Ping!')
   end
 end
