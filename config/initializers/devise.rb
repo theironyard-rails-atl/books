d9a23f7f5f548884dbd1a26eb2c971435f076148
Matthew Thompson <matt@mattathompson.org>
2014-09-17 13:39:32 -0400

    Finial Push for the Omniauth Feature

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,8 +229,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, :scope => 'email, read_stream'
-  config.omniauth :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], :scope => 'email, read_stream'
+  config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
