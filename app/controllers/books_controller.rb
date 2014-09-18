8aa209bcfb24b4a9ce9e00cea15303353f11f5da
Taylor Martin <taylormartin.uga@gmail.com>
2014-09-18 13:35:47 -0400

    working on save...again

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    binding.pry
     #TODO: add a check so that people can't add duplicate books
     @book = Book.new create_params.merge(creator: current_user)
     if @book.save
