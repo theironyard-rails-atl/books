72ed0339aa8e815241e320a9124d1c638a7abc72
James Dabbs <jamesdabbs@gmail.com>
2014-09-18 11:17:11 -0400

    Working book search specs

--- spec/services/book_search_spec.rb
+++ spec/services/book_search_spec.rb
@@ -6,5 +6,23 @@ describe BookSearch do
     expect( results.count ).to be >= 10
   end
 
-  it 'can search by author'
+  it 'can search by author' do
+    results = BookSearch.new('Potter', author: 'Rowling').run
+    expect( results.count ).to be >= 7
+
+    results.each do |result|
+      authors = result["volumeInfo"]["authors"]
+      # At least one author matches 'Rowling'
+      #match = authors.find { |a| a =~ /rowling/i }
+      #expect( match ).not_to be nil
+      names = authors.join ','
+      expect( names.downcase ).to include 'rowling'
+    end
+  end
+
+  it 'builds the right search query' do
+    # Maybe this should be different from the above setup?
+    q = BookSearch.new('Potter', author: 'Rowling').build_search_q
+    expect( q ).to eq 'Potter+inauthor:Rowling'
+  end
 end
