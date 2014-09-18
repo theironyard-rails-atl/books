20e41871cb40f4ff143dbbd0456865ca7ec95dee
James Dabbs <jamesdabbs@gmail.com>
2014-09-18 11:39:10 -0400

    Use :vcr tag :shipit:

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,3 +2,14 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
 end
+
+RSpec.configure do |c|
+  c.around :each, vcr: true do |example|
+    ex_description = example.metadata[:full_description]
+    # replace all whitespace with _s
+    cassette_name = ex_description.gsub /\s+/, '_'
+    VCR.use_cassette cassette_name do
+      example.run
+    end
+  end
+end
